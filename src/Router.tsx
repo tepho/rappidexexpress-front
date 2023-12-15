c5183462e6e639597012c30eadd04405bdadfd38
Matheus Morais <matheus.silva.morais01@gmail.com>
2023-12-15 10:22:52 -0300

    fix: adicionando layout default ao itens logados

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
+
 import { Login } from './pages/Login'
+
+import { DefaultLayout } from './layouts/DefaultLayout'
+import { Dashboard } from './pages/Dashboard'
 
 export function Router() {
     return (
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     )
   }
