6b49e6b984fb21f9ae5cf6f3a4a0dce57a9802dd
Matheus Morais <matheus.silva.morais01@gmail.com>
2023-12-15 09:44:01 -0300

    feat: criação da pagina de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,62 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { useForm } from 'react-hook-form'
+import { SignIn  } from 'phosphor-react'
+
+import { BaseButton, BaseInput, Container, FormContainer } from "./styles";
+
+const newLoginFormValidationSchema = zod.object({
+    user: zod.string().min(5, 'Informe o usuario.'),
+    password: zod
+      .string()
+      .min(4, 'Informe a senha.'),
+  })
+
+type NewLoginFormData = zod.infer<typeof newLoginFormValidationSchema>
+
 export function Login() {
+    const newLoginFormData = useForm<NewLoginFormData>({
+        resolver: zodResolver(newLoginFormValidationSchema),
+        defaultValues: {
+            user: '',
+            password: '',
+        },
+    })
+
+    const { handleSubmit, watch, reset, register } = newLoginFormData
+
+    function handleLogin(data: NewLoginFormData) {
+        console.log(data)
+        reset()
+    }
+
+    const user = watch('user')
+    const password = watch('password')
+    const isSubmitDisabled = !user || !password
+
     return (
-        <h1>Login</h1>
+        <Container> 
+            <form onSubmit={handleSubmit(handleLogin)} action="">
+                <FormContainer>
+                    <BaseInput
+                        type="text"
+                        id="user"
+                        placeholder="Informe o usuário."
+                        {...register('user')}
+                    />
+
+                    <BaseInput
+                        type="password"
+                        id="password"
+                        placeholder="Informe a senha."
+                        {...register('password')}
+                    />
+                </FormContainer>
+
+                <BaseButton disabled={isSubmitDisabled} type="submit">
+                    <SignIn size={24} /> Login
+                </BaseButton>
+            </form>
+        </Container>
     )
 }
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  flex: 1;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    margin-top: 22rem;
+  }
+`
+
+export const BaseButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 0.5rem;
+  font-weight: bold;
+  color: ${(props) => props.theme['gray-500']};
+
+  cursor: pointer;
+`
+
+export const FormContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: ${(props) => props.theme['gray-100']};
+  font-size: 1.125rem;
+  font-weight: bold;
+  
+  gap: 1rem;
+`
+
+export const BaseInput = styled.input`
+  flex: 1;
+  width: 20rem;
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  /* border-bottom: 2px solid ${(props) => props.theme['gray-500']}; */
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  &:focus {
+    box-shadow: none;
+    border: 2px solid ${(props) => props.theme['green-500']};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+`
