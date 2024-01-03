2edef4d59996f1da5bcba337ff097a05d7baf793
Matheus Morais <matheus.silva.morais01@gmail.com>
2024-01-03 10:47:32 -0300

    feat: estilos para pedidos atribuidos

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.main`
   flex: 1;
   width: 100%;
-  display: flex;
+  display: grid;
   align-items: center;
   justify-content: center;
 
@@ -11,7 +11,6 @@ export const Container = styled.main`
 `
 
 export const ContainerButtons = styled.div`
-  width: 30%;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -34,9 +33,81 @@ export const BaseButton = styled.div<ButtonProps>`
   align-items: center;
   justify-content: center;
 
-  gap: 0.5rem;
   font-weight: bold;
   color: ${(props) => props.typeReport ? props.theme['gray-900'] : props.theme['gray-500']};
 
   cursor: pointer;
 `
+
+export const ContainerDeliveries = styled.div`
+  background: ${(props) => props.theme['gray-500']};
+  min-width: 20rem;
+  border-radius: 1rem;
+`;
+
+export const Delivery = styled.div`
+  background-color: ${(props) => props.theme['green-700']};
+  padding: 1rem;
+  margin: 0.5rem;
+  border-radius: 10px;
+`;
+
+export const Link = styled.a`
+  display: flex;
+  gap: 0.2rem;
+  text-decoration: none;
+  color: ${(props) => props.theme['gray-300']};
+
+  font-weight: bold;
+`;
+
+export const ContainerShopkeeper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const ShopkeeperProfileImage = styled.img`
+  height: 6rem;
+  border-radius: 100%;
+  border: solid;
+`;
+
+export const ShopkeeperInfo = styled.div``;
+
+export const ContainerOrder = styled.div`
+  margin: 1rem;
+`;
+
+export const ContainerInfo = styled.div`
+  margin: 1rem;
+`;
+
+export const OrderActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+`;
+
+interface OrderProps {
+  typeButton: boolean
+}
+
+export const OrderButton = styled.div<OrderProps>`
+  /* background: ${(props) => props.theme['gray-800']}; */
+  background: ${(props) => props.typeButton ? props.theme['green-500'] : props.theme['red-700']};
+  border: none;
+  border-radius: 10%;
+
+  padding: 1rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-weight: bold;
+  color: ${(props) => props.theme['gray-300']};
+
+  cursor: pointer;
+`;
