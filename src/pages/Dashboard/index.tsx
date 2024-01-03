44746bcef6d819da7de64d8e43623de7fa317460
Matheus Morais <matheus.silva.morais01@gmail.com>
2024-01-03 10:47:46 -0300

    feat: estilos para pedidos atribuidos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,21 @@
 import { useState } from "react";
-import { BaseButton, Container, ContainerButtons } from "./styles";
+import { WhatsappLogo   } from 'phosphor-react'
+
+import { 
+    BaseButton, 
+    Container, 
+    ContainerButtons, 
+    ContainerInfo, 
+    ContainerDeliveries, 
+    ContainerOrder, 
+    ContainerShopkeeper, 
+    Delivery, 
+    Link, 
+    ShopkeeperInfo, 
+    ShopkeeperProfileImage, 
+    OrderActions,
+    OrderButton
+} from "./styles";
 
 export function Dashboard() {
     const [isFreeReport, setIsFreeReport] = useState(true)
@@ -16,6 +32,130 @@ export function Dashboard() {
                     <BaseButton typeReport={isFreeReport} onClick={onClickReportType}>Livres</BaseButton>
                     <BaseButton typeReport={assignedReport} onClick={onClickReportType}>Atribuídos</BaseButton>
             </ContainerButtons>
+            <ContainerDeliveries>
+                <Delivery>
+                    <ContainerShopkeeper>
+                        <ShopkeeperProfileImage src="https://pbs.twimg.com/profile_images/1696596355903332353/nWF46LFw_400x400.jpg" />
+                        <ShopkeeperInfo>
+                            <p>Toca</p>
+                            <Link href="">
+                                (94) 9 9210-4406 <WhatsappLogo size={18} />
+                            </Link>
+                            <Link href="">
+                                <p>Localização</p>
+                            </Link>
+                        </ShopkeeperInfo>
+                    </ContainerShopkeeper>
+
+                    <ContainerOrder>
+                        <p>Status: Livre</p>
+                        <p>Forma de pagamento: Pix</p>
+                        <p>Valor: R$ 70,00</p>
+                        <p>Pix: </p>
+                    </ContainerOrder>
+
+                    <ContainerInfo>
+                        <p>Cliente: Matheus </p>
+                        <Link href="">
+                            (94) 9 9210-4406 <WhatsappLogo size={18} />
+                        </Link>
+                    </ContainerInfo>
+
+                    <ContainerInfo>
+                        <p>Motoboy: Matheus </p>
+                        <Link href="">
+                            (94) 9 9210-4406 <WhatsappLogo size={18} />
+                        </Link>
+                    </ContainerInfo>
+
+                    <OrderActions>
+                        <OrderButton typeButton={true}>Atribuir</OrderButton>
+                        <OrderButton typeButton={false}>Apagar</OrderButton>
+                    </OrderActions>
+                </Delivery>
+
+                <Delivery>
+                    <ContainerShopkeeper>
+                        <ShopkeeperProfileImage src="https://pbs.twimg.com/profile_images/1696596355903332353/nWF46LFw_400x400.jpg" />
+                        <ShopkeeperInfo>
+                            <p>Toca</p>
+                            <Link href="">
+                                (94) 9 9210-4406 <WhatsappLogo size={18} />
+                            </Link>
+                            <Link href="">
+                                <p>Localização</p>
+                            </Link>
+                        </ShopkeeperInfo>
+                    </ContainerShopkeeper>
+
+                    <ContainerOrder>
+                        <p>Status: Livre</p>
+                        <p>Forma de pagamento: Pix</p>
+                        <p>Valor: R$ 70,00</p>
+                        <p>Pix: </p>
+                    </ContainerOrder>
+
+                    <ContainerInfo>
+                        <p>Cliente: Matheus </p>
+                        <Link href="">
+                            (94) 9 9210-4406 <WhatsappLogo size={18} />
+                        </Link>
+                    </ContainerInfo>
+
+                    <ContainerInfo>
+                        <p>Motoboy: Matheus </p>
+                        <Link href="">
+                            (94) 9 9210-4406 <WhatsappLogo size={18} />
+                        </Link>
+                    </ContainerInfo>
+
+                    <OrderActions>
+                        <OrderButton typeButton={true}>Atribuir</OrderButton>
+                        <OrderButton typeButton={false}>Apagar</OrderButton>
+                    </OrderActions>
+                </Delivery>
+
+                <Delivery>
+                    <ContainerShopkeeper>
+                        <ShopkeeperProfileImage src="https://pbs.twimg.com/profile_images/1696596355903332353/nWF46LFw_400x400.jpg" />
+                        <ShopkeeperInfo>
+                            <p>Toca</p>
+                            <Link href="">
+                                (94) 9 9210-4406 <WhatsappLogo size={18} />
+                            </Link>
+                            <Link href="">
+                                <p>Localização</p>
+                            </Link>
+                        </ShopkeeperInfo>
+                    </ContainerShopkeeper>
+
+                    <ContainerOrder>
+                        <p>Status: Livre</p>
+                        <p>Forma de pagamento: Pix</p>
+                        <p>Valor: R$ 70,00</p>
+                        <p>Pix: </p>
+                    </ContainerOrder>
+
+                    <ContainerInfo>
+                        <p>Cliente: Matheus </p>
+                        <Link href="">
+                            (94) 9 9210-4406 <WhatsappLogo size={18} />
+                        </Link>
+                    </ContainerInfo>
+
+                    <ContainerInfo>
+                        <p>Motoboy: Matheus </p>
+                        <Link href="">
+                            (94) 9 9210-4406 <WhatsappLogo size={18} />
+                        </Link>
+                    </ContainerInfo>
+
+                    <OrderActions>
+                        <OrderButton typeButton={true}>Atribuir</OrderButton>
+                        <OrderButton typeButton={false}>Apagar</OrderButton>
+                    </OrderActions>
+                </Delivery>
+            </ContainerDeliveries>
         </Container>
     )
 }
