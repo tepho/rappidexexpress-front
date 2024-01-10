542c864c224af78436ca7e389b447a8992704d30
Matheus Morais <matheus.silva.morais01@gmail.com>
2024-01-10 09:28:45 -0300

    fix: estilização da pagina de alterar senha

--- src/pages/ChangePassword/index.tsx
+++ src/pages/ChangePassword/index.tsx
@@ -1,5 +1,68 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { useForm } from 'react-hook-form'
+
+import { 
+    BaseButton,
+    BaseInput,
+    Container,
+    ContainerButtons,
+    FormContainer
+} from "./styles";
+
+const ChangePassFormValidationSchema = zod.object({
+    oldPassword: zod.string().min(4, 'Informe a senha antiga.'),
+    newPassword: zod.string().min(4, 'Informe a nova senha.'),
+  })
+
+  
+type ChangePassFormData = zod.infer<typeof ChangePassFormValidationSchema>
+
 export function ChangePassword() {
+    const changePasswordFormData = useForm<ChangePassFormData>({
+        resolver: zodResolver(ChangePassFormValidationSchema),
+        defaultValues: {
+            oldPassword: '',
+            newPassword: '',
+        },
+    })
+
+    function handleSave(data: ChangePassFormData) {
+        console.log(data)
+        // navigate('/dashboard')
+    }
+
+    const { handleSubmit, watch, register } = changePasswordFormData
+
+    const oldPassword = watch('oldPassword')
+    const newPassword = watch('newPassword')
+    const isSubmitDisabled = !oldPassword || !newPassword
+
     return (
-        <h1>Hello CHange Password</h1>
+        <Container>
+            <form onSubmit={handleSubmit(handleSave)} action="">
+                <FormContainer>
+                    <label htmlFor="name">Senha antiga:</label>
+                    <BaseInput
+                        type="password"
+                        id="oldPassword"
+                        placeholder="Informe a senha antiga."
+                        {...register('oldPassword')}
+                    />
+
+                    <label htmlFor="name">Nova senha:</label>
+                    <BaseInput
+                        type="password"
+                        id="newPassword"
+                        placeholder="Informe a nova senha."
+                        {...register('newPassword')}
+                    />
+
+                    <ContainerButtons>
+                        <BaseButton disabled={isSubmitDisabled} type="submit">Salvar</BaseButton>
+                    </ContainerButtons>
+                </FormContainer>
+            </form>
+        </Container>
     )
 }
