2a4ba4158b19f97d9ee97c4ea350db4afdef6ccc
Matheus Morais <matheus.silva.morais01@gmail.com>
2024-01-11 11:15:38 -0300

    feat: criação do reducer de delivery

---
+++ src/reducers/delivery/reducer.ts
@@ -0,0 +1,25 @@
+import { produce } from 'immer'
+import { ActionTypes } from './actions'
+
+interface DeliveryState {
+    token: string | null,
+    permission: string | null
+}
+
+export function deliveryReducer(state: DeliveryState, action: any) {
+    switch (action.type) {
+        case ActionTypes.LOGIN:
+            return produce(state, (draft) => {
+                draft.token = action.payload.token
+                draft.permission = action.payload.permission
+            })
+        case ActionTypes.LOGOUT:
+            return produce(state, (draft) => {
+                console.log('teste')
+                draft.token = null
+                draft.permission = null
+            })
+        default:
+            return state
+    }
+}
