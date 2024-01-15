4d0a954760b0e5a348ab8a40453a4e9411ef445b
Matheus Morais <matheus.silva.morais01@gmail.com>
2024-01-15 09:13:00 -0300

    fix: diminuindo o margin e padding do default layout

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
-  margin: 1rem;
-  padding: 2.5rem;
+  margin: 0.5rem;
+  padding: 1rem;
 
   background: ${(props) => props.theme['gray-800']};
   border-radius: 8px;
