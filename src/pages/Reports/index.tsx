23b9325a0c0385c608d3cd935d47151fba29a477
Matheus Morais <matheus.silva.morais01@gmail.com>
2024-01-16 09:48:18 -0300

    feat: conectando reports to api

--- src/pages/Reports/index.tsx
+++ src/pages/Reports/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { 
     Container, 
@@ -14,17 +14,66 @@ import {
     ShopkeeperInfo, 
     ShopkeeperProfileImage 
 } from "./styles";
+import api from "../../services/api";
+import { DeliveryContext } from "../../context/DeliveryContext";
 
 export function Reports() {
+    const { token } = useContext(DeliveryContext)
+    api.defaults.headers.Authorization = `Bearer ${token}`
+    
+    
+    const [motoboys, setMotoboys] = useState([]);
+    const [shopkeepers, setShopkeepers] = useState([]);
+
+    const [reports, setReports] = useState([]);
+    const [reportsAmount, setReportsAmount] = useState(0);
+
     const [selectedStatus, setSelectedStatus] = useState('FINALIZADO');
     const [selectedMotoboy, setSelectedMotoboy] = useState('');
     const [selectedEstablishment, setSelectedEstablishment] = useState('');
     const [createdIn, setCreatedIn] = useState('');
     const [createdUntil, setCreatedUntil] = useState('');
 
-    function onClickSearch(){
+    async function onClickSearch(){
         console.log({selectedStatus, selectedMotoboy, selectedEstablishment, createdIn, createdUntil});
+        let param = '';
+        if(selectedMotoboy){
+            param = `${param}&motoboyId=${selectedMotoboy}`
+        }
+        if(selectedEstablishment){
+            param = `${param}&establishmentId=${selectedEstablishment}`
+        }
+        if(createdIn){
+            param = `${param}&createdIn=${createdIn}`
+        }
+        if(createdUntil){
+            param = `${param}&createdUntil=${createdUntil}`
+        }
+
+        try {
+            const response = await api.get(`/delivery?status=${selectedStatus}${param}`)
+            setReports(response.data.data)
+            setReportsAmount(response.data.count)
+        } catch (error) {
+            alert(error.response.data.message)
+        }
     }
+
+    async function getData(){
+        try {
+            const motoboysResponse = await api.get('/user?type=motoboy')
+            const shopkeepersResponse = await api.get('/user?type=shopkeeper')
+
+            setMotoboys(motoboysResponse.data.data)
+            setShopkeepers(shopkeepersResponse.data.data)
+        } catch (error) {
+            alert(error.response.data.message)
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    })
 
     return (
         <Container>
@@ -59,9 +108,12 @@ export function Reports() {
                         value={selectedMotoboy}
                         onChange={e => setSelectedMotoboy(e.target.value)}
                     >
-                        <option value="motoboy1">Valor 1</option>
-                        <option value="motoboy2">Valor 2</option>
-                        <option value="motoboy3">Valor 3</option>
+                        <option value=''>Todos</option>
+                        {
+                            motoboys.map(motoboy => 
+                                <option key={motoboy.id} value={motoboy.id}>{motoboy.name}</option>
+                            )
+                        }
                     </select>           
                 </Filter>
 
@@ -71,9 +123,12 @@ export function Reports() {
                         value={selectedEstablishment}
                         onChange={e => setSelectedEstablishment(e.target.value)}
                     >
-                        <option value="establishment1">Valor 1</option>
-                        <option value="establishment2">Valor 2</option>
-                        <option value="establishment3">Valor 3</option>
+                        <option value=''>Todos</option>
+                        {
+                            shopkeepers.map(shopkeeper => 
+                                <option key={shopkeeper.id} value={shopkeeper.id}>{shopkeeper.name}</option>
+                            )
+                        }
                     </select>           
                 </Filter>
 
@@ -81,8 +136,36 @@ export function Reports() {
             </FiltersContainer>
 
             <ReportsContainer>
-                <h2>Quantidade de entregas: 99</h2>
-                <Delivery>
+                <h3>Quantidade de entregas: {reportsAmount}</h3>
+                {reports.map((report) => 
+                    <Delivery key={report.id}>
+                        <ContainerShopkeeper>
+                            <ShopkeeperProfileImage src="https://pbs.twimg.com/profile_images/1696596355903332353/nWF46LFw_400x400.jpg" />
+                            <ShopkeeperInfo>
+                                <p>{report.establishmentName}</p>
+                                {report.establishmentPhone}
+                            </ShopkeeperInfo>
+                        </ContainerShopkeeper>
+                        <ContainerOrder>
+                            <p>Status: {report.status}</p>
+                            <p>Forma de pagamento: {report.payment}</p>
+                            <p>Valor: R$ {report.value}</p>
+                            <p>Pix: </p>
+                        </ContainerOrder>
+
+                        <ContainerInfo>
+                            <p>Cliente: {report.clientName} </p>
+                                {report.clientPhone}
+                        </ContainerInfo>
+
+                        <ContainerInfo>
+                            <p>Motoboy: {report.motoboyName} </p>
+                            {report.motoboyPhone}
+                        </ContainerInfo>
+                    </Delivery>
+                )}
+
+                {/* <Delivery>
                     <ContainerShopkeeper>
                         <ShopkeeperProfileImage src="https://pbs.twimg.com/profile_images/1696596355903332353/nWF46LFw_400x400.jpg" />
                         <ShopkeeperInfo>
@@ -158,33 +241,7 @@ export function Reports() {
                         <p>Motoboy: Matheus </p>
                         (94) 9 9210-4406
                     </ContainerInfo>
-                </Delivery>
-
-                <Delivery>
-                    <ContainerShopkeeper>
-                        <ShopkeeperProfileImage src="https://pbs.twimg.com/profile_images/1696596355903332353/nWF46LFw_400x400.jpg" />
-                        <ShopkeeperInfo>
-                            <p>Toca</p>
-                            (94) 9 9210-4406
-                        </ShopkeeperInfo>
-                    </ContainerShopkeeper>
-                    <ContainerOrder>
-                        <p>Status: Livre</p>
-                        <p>Forma de pagamento: Pix</p>
-                        <p>Valor: R$ 70,00</p>
-                        <p>Pix: </p>
-                    </ContainerOrder>
-
-                    <ContainerInfo>
-                        <p>Cliente: Matheus </p>
-                            (94) 9 9210-4406
-                    </ContainerInfo>
-
-                    <ContainerInfo>
-                        <p>Motoboy: Matheus </p>
-                        (94) 9 9210-4406
-                    </ContainerInfo>
-                </Delivery>
+                </Delivery> */}
             </ReportsContainer>
         </Container>
     )
