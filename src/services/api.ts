1bc5485f299d0dfb26e1527efec319f0b48394cc
Matheus Morais <matheus.silva.morais01@gmail.com>
2024-02-08 10:44:17 -0300

    fix: config da url da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
-let api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+let apiUrl = 'http://localhost:3000/api'
+if (process.env.ENVIRIONMENT === 'PROD'){
+    apiUrl = 'https://rappidex-api-eef82025324b.herokuapp.com/api'
+}
+
+const api = axios.create({
+    
+    baseURL: apiUrl,
 });
 
 export default api;
