bf6e9978c6908cb522de2c2961d507cbef87d423
Matheus Morais <matheus.silva.morais01@gmail.com>
2024-02-08 10:52:28 -0300

    fix: mudando o tipo de arquivo

--- server.mjs
+++ server.mjs
@@ -1,15 +1,8 @@
 /* eslint-disable no-undef */
 import express from 'express'
-import { resolve } from 'path'
-
 const app = express()
 
-app.use('/', express.static(
-    resolve(
-        __dirname,
-        './dist'
-    )
-))
+app.use('/', express.static('./dist'))
 
 app.listen(process.env.PORT || 3000, (err) => {
     if(err) { return console.log(err) }
