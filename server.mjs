fd6c297ce686dc015f002b8edb7a8b9851457829
Matheus Morais <matheus.silva.morais01@gmail.com>
2024-02-09 16:18:08 -0300

    fix: corrigindo rotas no server mjs

--- server.mjs
+++ server.mjs
@@ -3,6 +3,7 @@ import express from 'express'
 const app = express()
 
 app.use('/', express.static('./dist'))
+app.use('*', express.static('./dist'))
 
 app.listen(process.env.PORT || 3000, (err) => {
     if(err) { return console.log(err) }
