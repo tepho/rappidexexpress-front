02b352aced35d8b64b6f3c8d3a4a1aed9399f5e8
Matheus Morais <matheus.silva.morais01@gmail.com>
2024-02-22 11:49:56 -0300

    fix: add init to onesignal to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { useEffect } from 'react'
+import OneSignal from 'react-onesignal';
 
 import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
@@ -7,6 +9,12 @@ import { defaultTheme } from './styles/themes/default'
 import { DeliveryContextProvider } from './context/DeliveryContext'
 
 function App() {
+
+  useEffect(() => {
+    OneSignal.init({ appId: 'b0d375dc-8f89-4bee-ac54-0a04fef00ebc' }).then(() => {
+      OneSignal.Slidedown.promptPush();
+    })
+  })
 
   return (
     <ThemeProvider theme={defaultTheme}>
