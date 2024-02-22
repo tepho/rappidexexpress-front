0c12ae0eb1076ba8c813dbe90b796723f606f25e
Matheus Morais <matheus.silva.morais01@gmail.com>
2024-02-22 15:49:55 -0300

    fix: install and activate the sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,11 @@
-import { precacheAndRoute } from 'workbox-precaching';
+import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
 precacheAndRoute(self.__WB_MANIFEST || []);
+// Clean up old cache
+cleanupOutdatedCaches();
+
+// Install and activate service worker
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
 
 self.addEventListener('push', function(event) {
     const body = event.data?.text() ?? ''
