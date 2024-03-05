ff8b16d235d376fdc80414e43043bec5bec951fb
Matheus Morais <matheus.silva.morais01@gmail.com>
2024-03-05 17:41:14 -0300

    feat: add pagina para configurações

---
+++ src/pages/Config/index.tsx
@@ -0,0 +1,109 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useContext, useEffect, useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { useForm } from 'react-hook-form'
+
+import { 
+    BaseButton,
+    BaseInput,
+    Container,
+    ContainerButtons,
+    FormContainer
+} from "./styles";
+import { Loader } from '../../components/Loader';
+import api from '../../services/api';
+import { DeliveryContext } from '../../context/DeliveryContext';
+
+const AmountFormValidationSchema = zod.object({
+    amount: zod.string(),
+  })
+
+  
+type AmountFormData = zod.infer<typeof AmountFormValidationSchema>
+
+export function Config() {
+    const { token } = useContext(DeliveryContext)
+    api.defaults.headers.Authorization = `Bearer ${token}`
+    
+    const [loading, setLoading] = useState(true)
+    const [loadingButton, setLoadingButton] = useState(false)
+    const [formValues, setFormValues] = useState({
+        amount: '0',
+    })
+
+    const amountFormData = useForm<AmountFormData>({
+        resolver: zodResolver(AmountFormValidationSchema),
+        values: formValues,
+    })
+
+    const { handleSubmit, watch, register } = amountFormData
+
+    async function handleSave() {
+        if(loadingButton) {
+            return
+        }
+
+        if(parseInt(amount) === 0){
+            alert('Você precisa digitar um valor!')
+            return
+        } 
+        setLoadingButton(true)
+        try {
+            await api.put(`/delivery/config/${amount}`)
+            setLoadingButton(false)
+            alert("Número de entregas por motoboy alterada com sucesso!")
+        } catch (error: any) {
+            setLoadingButton(false)
+            alert(error.response.data.message)
+        }
+    }
+
+    async function getConfigs() {
+        try {
+            const configs = await api.get(`/delivery/config`)
+            setFormValues({ amount: configs.data.amount })
+            setLoading(false)
+        } catch (error: any) {
+            setLoading(false)
+            alert(error.response.data.message)
+        }
+    }
+
+    const amount = watch('amount')
+    const isSubmitDisabled = !amount
+
+    useEffect(() => {
+        if(loading){
+            getConfigs()
+        }
+    })
+
+    return (
+        <Container>
+            {loading ? 
+                <Loader size={10} biggestColor='gray' smallestColor='gray' /> :
+                <form onSubmit={handleSubmit(handleSave)} action="">
+                    <FormContainer>
+                        <label htmlFor="amount">Numero de entregas por motoboy:</label>
+                        <BaseInput
+                            type="number"
+                            id="amount"
+                            placeholder="Informe a quantidade."
+                            {...register('amount')}
+                        />
+
+                        <ContainerButtons>
+                            <BaseButton disabled={isSubmitDisabled} type="submit">
+                                {loadingButton ?
+                                    <Loader size={20} biggestColor='gray' smallestColor='gray' /> :
+                                    "Salvar"
+                                }
+                            </BaseButton>
+                        </ContainerButtons>
+                    </FormContainer>
+                </form>
+            }
+        </Container>
+    )
+}
