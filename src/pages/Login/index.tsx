c3cf61a0db7082769af2502b35199ff1725311cb
Matheus Morais <matheus.silva.morais01@gmail.com>
2024-03-07 11:06:33 -0300

    fix: add promptPush to runOneSignal function

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -42,11 +42,11 @@ export function Login() {
     async function runOneSignal(username: string, token: string){
         api.defaults.headers.Authorization = `Bearer ${token}`
         // console.log(OneSignal)
-        // if(OneSignal && OneSignal?.User?.PushSubscription?.id){
-        //     await api.put(`/user/${username}/notification-config`, { notification: { subscriptionId: OneSignal.User.PushSubscription.id } })
-        //     return
-        // }
-        // await OneSignal.Slidedown.promptPush();
+        if(OneSignal && OneSignal.User && OneSignal.User.PushSubscription && OneSignal.User.PushSubscription.id){
+            await api.put(`/user/${username}/notification-config`, { notification: { subscriptionId: OneSignal.User.PushSubscription.id } })
+            return
+        }
+        await OneSignal.Slidedown.promptPush();
         await api.put(`/user/${username}/notification-config`, { notification: { subscriptionId: OneSignal.User.PushSubscription.id } })
     }
 
