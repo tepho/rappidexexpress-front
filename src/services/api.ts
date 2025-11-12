e9af0661f413808cdfbe820ce1536372f8440263
tepho <stephano_gomes@hotmail.com>
2025-11-12 02:03:18 -0300

    Feat. adição de cidades no cadastros

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'http://localhost:4000/api'
-// const apiUrl = 'https://rappidex-api-eef82025324b.herokuapp.com/api'
+const apiUrl = 'https://rappidex-api-eef82025324b.herokuapp.com/api'
 
 const api = axios.create({
     
