102b52ba4ce0b17ec39c452cd3fb9d9ea53cb695
Warlles Noleto <142702004+WarllesNoleto@users.noreply.github.com>
2026-04-07 01:40:53 -0300

    Atualização warlles
    adicionando a opção de mensagem personalizada por cidades e etc...

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -4,29 +4,28 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useNavigate } from 'react-router-dom';
 import * as zod from 'zod'
 import { useForm } from 'react-hook-form'
-import { SignIn  } from 'phosphor-react'
+import { SignIn } from 'phosphor-react'
 
 import { DeliveryContext } from '../../context/DeliveryContext';
 import { BaseButton, BaseInput, Container, FormContainer, Logo } from "./styles";
 import { Loader } from '../../components/Loader';
 
 import api from '../../services/api';
-import OneSignal from 'react-onesignal';
-// import OneSignal from 'react-onesignal';
+import { getPushSubscriptionId, isOneSignalEnabled, isPushOptedIn } from '../../services/onesignal';
 
 const newLoginFormValidationSchema = zod.object({
     user: zod.string().min(3,'Informe o usuario.'),
     password: zod
       .string()
       .min(4, 'Informe a senha.'),
-  })
+})
 
 type NewLoginFormData = zod.infer<typeof newLoginFormValidationSchema>
 
 export function Login() {
-
     const { login } = useContext(DeliveryContext)
     const navigate = useNavigate()
+
     const newLoginFormData = useForm<NewLoginFormData>({
         resolver: zodResolver(newLoginFormValidationSchema),
         defaultValues: {
@@ -39,57 +38,48 @@ export function Login() {
 
     const { handleSubmit, watch, reset, register } = newLoginFormData
 
-    async function runOneSignal(username: string, token: string){
-        api.defaults.headers.Authorization = `Bearer ${token}`
-        // console.log(OneSignal)
-        if(OneSignal && OneSignal.User && OneSignal.User.PushSubscription && OneSignal.User.PushSubscription.id){
-            await api.put(`/user/${username}/notification-config`, { notification: { subscriptionId: OneSignal.User.PushSubscription.id } })
-            return
-        }
-        await OneSignal.Slidedown.promptPush();
-        await api.put(`/user/${username}/notification-config`, { notification: { subscriptionId: OneSignal.User.PushSubscription.id } })
+    async function runOneSignal(username: string, token: string) {
+    if (!isOneSignalEnabled) {
+        return
     }
 
-    // async function configureNotification(user: string, token: string){
-    //     console.log({
-    //         teste: 'novo mensageiro',
-    //         user,
-    //     })
-    //     navigator.serviceWorker.register('service-worker.js').then(async serviceWorker => {
-    //         api.defaults.headers.Authorization = `Bearer ${token}`
-    //         // let subscription = await serviceWorker.pushManager.
-    //         const publicKey = 'BAGRiJjxkVtCYwNHdUM9amJDdXecxMK73KLZQgEOUrT7sDMvcROMxA5Utzsbx1noeFOZz8R9yu7U3_1nuCfcQDY';
-    //         const subscription = await serviceWorker.pushManager.subscribe({
-    //           userVisibleOnly: true,
-    //           applicationServerKey: publicKey,
-    //         })
-    //         // if (!subscription){
-    //         // }
+    try {
+        api.defaults.headers.Authorization = `Bearer ${token}`
 
-    //         console.log(subscription)
+        const optedIn = await isPushOptedIn()
+        const subscriptionId = await getPushSubscriptionId()
 
-    //         await api.put(`/user/${user}/notification-config`, { notification: subscription })
-    //       })
-    // }
+        if (!optedIn || !subscriptionId) {
+            return
+        }
+
+        await api.put(`/user/${username}/notification-config`, {
+            notification: { subscriptionId }
+        })
+    } catch (error) {
+        console.log('Falha ao sincronizar notificação no login.', error)
+    }
+}
 
     async function handleLogin(data: NewLoginFormData) {
-        if(loading) {
+        if (loading) {
             return
         }
 
         setLoading(true)
+
         try {
-            const reponse = await api.post('/auth', data)
-            login(reponse.data.token, reponse.data.permission)
-            console.log({data: "run onesignal with", user: data.user, token: reponse.data.token})
-            await runOneSignal(data.user, reponse.data.token)
-            // await configureNotification(data.user, reponse.data.token)
+            const response = await api.post('/auth', data)
+
+            login(response.data.token, response.data.permission)
+            await runOneSignal(data.user, response.data.token)
+
             reset()
             navigate('/')
-            setLoading(false)
         } catch (error: any) {
+            alert(error.response?.data?.message ?? 'Erro ao fazer login.')
+        } finally {
             setLoading(false)
-            alert(error.response.data.message)
         }
     }
 
@@ -98,10 +88,10 @@ export function Login() {
     const isSubmitDisabled = !user || !password
 
     return (
-        <Container> 
+        <Container>
             <form onSubmit={handleSubmit(handleLogin)} action="">
                 <FormContainer>
-                    <Logo src="https://i.pinimg.com/736x/a5/9f/17/a59f176343c6fd0d83adea72eaf0c57f.jpg"  />
+                    <Logo src="https://i.pinimg.com/736x/a5/9f/17/a59f176343c6fd0d83adea72eaf0c57f.jpg" />
                     <BaseInput
                         type="text"
                         id="user"
@@ -118,12 +108,13 @@ export function Login() {
                 </FormContainer>
 
                 <BaseButton disabled={isSubmitDisabled} type="submit">
-                    { !loading ?
+                    {!loading ? (
                         <>
-                            <SignIn size={24} /> Login 
-                        </> :
+                            <SignIn size={24} /> Login
+                        </>
+                    ) : (
                         <Loader size={20} biggestColor='black' smallestColor='green' />
-                    }
+                    )}
                 </BaseButton>
             </form>
         </Container>
